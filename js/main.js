aa4ad2a3125de50405189a4820d93619158fbf0d
Dronandre <dronandre@yandex.ru>
2020-09-02 14:29:14 +0700

    Add modalwindow and features

--- js/main.js
+++ js/main.js
@@ -92,5 +92,52 @@ window.addEventListener("DOMContentLoaded", function () {
 	}
 
 	setClock('.timer', deadline);
+
+	// modal
+
+	const modalTrigger = document.querySelectorAll('[data-modal]'),
+		  modal = document.querySelector('.modal'),
+		  modalCloseBtn = document.querySelector('[data-close]');
+	
+	function openModal(){
+		modal.classList.toggle('show');
+		document.body.style.overflow = 'hidden';
+		clearInterval(modalTimerId);
+	}
+	
+	modalTrigger.forEach(item => {
+		item.addEventListener('click', openModal);
+	});
+
+	function closeModal(){
+		modal.classList.toggle('show');
+		document.body.style.overflow = '';
+	}
+
+	modalCloseBtn.addEventListener('click', closeModal);
+
+	modal.addEventListener('click', (e) => {
+		if (e.target === modal) {
+			closeModal();
+		}
+	});
+
+	document.addEventListener('keydown', (e)=> {
+		if (e.code === 'Escape' && modal.classList.contains('show')) {
+			closeModal();			
+		}
+	});
+
+	const modalTimerId = setTimeout(openModal, 6000);
+
+	function showModalByScroll() {
+		if (window.pageYOffset + document.documentElement.clientHeight >=  document.documentElement.scrollHeight) {
+			openModal();
+			window.removeEventListener('scroll', showModalByScroll);
+		} 
+	}
+
+	window.addEventListener('scroll', showModalByScroll);
+
 });
 
