18a902c4e505484a47d901d944cbd5c4ffba9344
Dronandre <dronandre@yandex.ru>
2020-10-05 20:34:13 +0700

    webpack

---
+++ js/script.js
@@ -0,0 +1,33 @@
+import tabs from './modules/tabs';
+import modal, { openModal } from './modules/modal';
+import cards from './modules/cards';
+import forms from './modules/forms';
+import timer from './modules/timer';
+import slider from './modules/slider';
+import calc from './modules/calc';
+
+
+window.addEventListener("DOMContentLoaded", function () {
+
+	const modalTimerId = setTimeout(() => openModal('.modal', modalTimerId), 50000);
+
+	tabs(".tabheader__item", ".tabcontent", ".tabheader__items", "tabheader__item_active");
+	modal("[data-modal]", ".modal", modalTimerId);
+	cards();
+	forms('form', modalTimerId);
+	timer('.timer', '2020-10-15');
+	slider({  
+		container: '.offer__slider',
+		slide: '.offer__slide',
+		nextArrow: '.offer__slider-next',
+		prevArrow: '.offer__slider-prev',
+		totalCounter: '#total',
+		currentCounter: '#current',
+		wrapper: '.offer__slider-wrapper',
+		field: '.offer__slider-inner'
+	});
+	calc();
+		
+});
+
+
